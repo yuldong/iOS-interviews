66896893f5e94a2b51114e1087a9e0da712b5e7c
yuldong <yuldong@126.com>
2020-04-11 23:14:42 +0800

    demo 增加wkwebview的代码，初步

--- Answers/Native_JS.md
+++ Answers/Native_JS.md
@@ -1,6 +1,30 @@
 ## native 与 js 
 
-
+---
 ### WKWebview 与 js
+**WKScriptMessageHandler, WKUIDelegate, WKNavigationDelegate**
+- WKNavigationDelegate 
+
+
+
+
+- WKUIDelegate
+
+
+
+- WKScriptMessageHandler
+  
+  `遵守该协议的类提供一个用于接受来自webpage中js执行传递的信息`
+
+  
+
+
+
+
 
 ### Javascriptcore
+
+
+
+### 参考
+- [WKWebView使用JS与Swift交互](https://tomoya92.github.io/2018/07/05/swift-webview-javascript/)
--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKScriptMessageHandler, WKUIDelegate, WKNavigationDelegate {
 
+    let html = try! String(contentsOfFile: Bundle.main.path(forResource: "index", ofType: "html")!, encoding: .utf8)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(webV)
+        webV.loadHTMLString(html, baseURL: nil)
+    }
+    
+    lazy var webV: WKWebView = {
+        let preference = WKPreferences()
+        preference.javaScriptEnabled = true
         
+        let configuration = WKWebViewConfiguration()
+        configuration.preferences = preference
+        configuration.userContentController = WKUserContentController()
+        configuration.userContentController.add(self, name: "AppModel")
+        
+        var webV = WKWebView(frame: self.view.bounds, configuration: configuration)
+        webV.uiDelegate = self
+        webV.navigationDelegate = self
+        return webV
+    }()
+}
+
+extension ViewController {
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        print(message.body)
     }
 }
 
